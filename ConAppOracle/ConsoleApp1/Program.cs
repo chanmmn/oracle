22124ff44f9fa0940b6779cd5e1e3db7c91caa87
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-02-17 09:04:54 -0800

    Commit ConAppOracle
    Commit ConAppOracle

---
+++ ConAppOracle/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace OracleDatabaseAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string conString = "User Id=system;Password=Pa$$w0rd;Data Source=scott";
+            string conString = "User Id=SYSTEM;Password=password;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=scott)))";
+
+            using (OracleConnection con = new OracleConnection(conString))
+            {
+                try
+                {
+                    con.Open();
+                    Console.WriteLine("Connected to Oracle database.");
+
+                    string sqlQuery = "SELECT * FROM EMP";
+
+                    using (OracleCommand cmd = new OracleCommand(sqlQuery, con))
+                    {
+                        using (OracleDataReader reader = cmd.ExecuteReader())
+                        {
+                            Console.WriteLine("EMPNO\tENAME\tJOB\t\tMGR\tHIREDATE\tSAL\tCOMM\tDEPTNO");
+                            while (reader.Read())
+                            {
+                                Console.WriteLine($"{reader["EMPNO"]}\t{reader["ENAME"]}\t{reader["JOB"]}\t{reader["MGR"]}\t{reader["HIREDATE"]}\t{reader["SAL"]}\t{reader["COMM"]}\t{reader["DEPTNO"]}");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
+        }
+    }
+}
